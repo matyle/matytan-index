2ededf31cb4dbec02141de90b316b243175001c6
MatyTan <matytan@bituniverse.org>
2024-02-27 17:41:14 +0800

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: "Matytan|勒勒乐了",
+  title: "勒勒乐了",
   tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
 
